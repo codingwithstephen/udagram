e83f51424c5c976c6d35f0043f98497b84f1fc51
stevemooney123 <stephen@kaizenappz.com>
2023-01-21 10:01:27 +0000

    first commit
    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=stephen
+export POSTGRES_PASSWORD=database
+export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-bucket-4118089432728
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
